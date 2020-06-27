e992f08d2a09f5ed69a224445cc0761b6a84b265
Long Nguyen <n.h.long.9697@gmail.com>
2020-06-27 13:22:13 +0700

    finish SignUpServlet

---
+++ src/controller/SignUpServlet.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.Student;
+import service.StudentDAO;
+import service.StudentService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SignUpServlet")
+public class SignUpServlet extends HttpServlet {
+    private final StudentService studentService = new StudentDAO();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String className = request.getParameter("className");
+        Student newStudent = new Student(username,password,firstName,lastName,email,className);
+        this.studentService.add(newStudent);
+        request.setAttribute("message","new product is added");
+        try {
+            request.getRequestDispatcher("signup.jsp").forward(request,response);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            request.getRequestDispatcher("signup.jsp").forward(request,response);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
