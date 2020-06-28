6681465bb21a3e5289e7cd77cd3a653af237e95c
Long Nguyen <n.h.long.9697@gmail.com>
2020-06-28 22:15:13 +0700

    add class model

--- src/model/Class.java
+++ src/model/Class.java
@@ -3,6 +3,27 @@ package model;
 import java.util.List;
 
 public class Class {
-    String className;
-    List<Student> students;
+    private String className;
+    private List<Student> students;
+
+    public Class(String className, List<Student> students) {
+        this.className = className;
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
